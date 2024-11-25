32b8147732e5d7d213ad3ba5d280cfbcef203d91
Nikita Tikhonov <jagarnikita@gmail.com>
2024-11-25 17:57:12 +0100

    add view binding

--- app/src/main/java/com/nikitaTikhonov/diemusik/ui/OnboardingFragment.kt
+++ app/src/main/java/com/nikitaTikhonov/diemusik/ui/OnboardingFragment.kt
@@ -5,13 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.nikitaTikhonov.diemusik.R
+import com.nikitaTikhonov.diemusik.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nameInput.setText("Some text")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
