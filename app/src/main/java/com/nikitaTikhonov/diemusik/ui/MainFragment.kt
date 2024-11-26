09a8e8f337d9967a6aebdf488b1aa6ed419ec2f5
Nikita Tikhonov <jagarnikita@gmail.com>
2024-11-26 15:20:30 +0100

    add basic navigation graph

---
+++ app/src/main/java/com/nikitaTikhonov/diemusik/ui/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.nikitaTikhonov.diemusik.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nikitaTikhonov.diemusik.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/nikitaTikhonov/diemusik/ui/OnboardingFragment.kt
+++ app/src/main/java/com/nikitaTikhonov/diemusik/ui/OnboardingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.nikitaTikhonov.diemusik.R
 import com.nikitaTikhonov.diemusik.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
@@ -25,10 +27,17 @@ class OnboardingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.nameInput.setText("Some text")
+        binding.onboardingToToMainFragmentButton.setOnClickListener {
+            goToMainFragment()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun goToMainFragment() {
+        findNavController().navigate(R.id.action_onboardingFragment_to_mainFragment)
     }
 }
